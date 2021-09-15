ee4922e8c1c67dc94b232f46b8b1b34544f7184a
deng <6882855@qq.com>
2021-09-15 09:41:16 +0800

    t

--- src/main/java/com/planb/common/conf/WebConfig.java
+++ src/main/java/com/planb/common/conf/WebConfig.java
@@ -1,12 +1,15 @@
-package com.planb.common.controller;
+package com.planb.common.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.planb.common.controller.MyHandlerInterceptor;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
     @Autowired
     private MyHandlerInterceptor myHandlerInterceptor;
     
@@ -15,3 +18,4 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addInterceptor(myHandlerInterceptor);
 	}
 }
+
