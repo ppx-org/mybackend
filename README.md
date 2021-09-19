d4e801a78320773a1bdbc9e833a21e007b032f9e
mark <6882855@qq.com>
2021-09-19 15:25:48 +0800

    Signed-off-by: mark <6882855@qq.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # mybackend
 mybackend
 
-test-V0.0.4
+test-V0.0.5
--- doc/sql/auth.sql
+++ doc/sql/auth.sql
@@ -1,41 +1,73 @@
 
+-- 缓存
+create table auth_user_jwt_cache {
+	user_id int not null,
+	jwt_version int not null
+	primary key(user_id)
+}
+comment on table auth_user_jwt_cache is 'jwt缓存,放redis';
+comment on column auth_user_jwt_cache.jwt_version is '修改密码、作废、修改角色等，需要换token';
 
+create table auth_version_cache {
+	auth_key varchar(32) not null,
+	auth_version int not null,
+	primary key(auth_key)
+}
+comment on table auth_version_cache is '权限版本,放redis';
+comment on column auth_version_cache.auth_version is '修改权限时，需要刷新内存';
+
+
+
+-- >>>>>>>>>>>> 权限
 create table auth_user (
 	user_id serial not null,
 	user_name varchar(32) not null,
-	user_password varchar(32) not null
-)
-comment on table auth_user is 'auth_user';
+	user_password varchar(32) not null,
+	primary key(user_id),
+	unique(user_name)
+);
+comment on table auth_user is '用户';
 
 create table auth_role (
 	role_id serial not null,
-	role_name varchar(32) not null
+	role_name varchar(32) not null,
+	primary key(role_id)
 )
-comment on table auth_role is 'auth_role';
+comment on table auth_role is '角色';
 
 create table auth_user_in_role (
-
-)
-
+	user_id int not null,
+	role_id int not null,
+	primary key(user_id, role_id)
+);
+comment on table auth_user_in_role is '用户所属角色';
 
 create table auth_res (
 	res_id serial not null,
 	res_name varchar(32) not null,
 	res_type varchar(1) not null,
 	res_menu_uri_id int,
-)
-comment on table auth_res is 'auth_res';
-comment on column auth_role.res_type is '类型 目录、菜单、操作';
+	primary key(res_id)
+);
+comment on table auth_res is '权限资源';
+comment on column auth_role.res_type is '类型:目录、菜单、操作';
 
 create table auth_uri (
 	uri_id serial not null,
-	uri varchar(64) not null
-)
-comment on table auth_uri is 'auth_uri';
+	uri_path varchar(64) not null,
+	primary key(user_id, role_id)
+);
+comment on table auth_uri is 'URI(path)';
 
 create table auth_res_uri (
-	
-)
+	res_id int not null,
+	uri_id int not null,
+	primary(res_id, uri_id)
+);
+comment on table auth_res_uri is '资源拥有的URI';
+
+
+
 
 
 
