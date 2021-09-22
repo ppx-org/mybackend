36108ad787ee19807e1d4e130b75d7d675d38688
deng <6882855@qq.com>
2021-09-22 11:09:59 +0800

    t

--- src/main/java/com/planb/RunApplication.java
+++ src/main/java/com/planb/RunApplication.java
@@ -1,56 +1,15 @@
 package com.planb;
 
-import java.security.SecureRandom;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.JWSObject;
-import com.nimbusds.jose.JWSVerifier;
-import com.nimbusds.jose.Payload;
-import com.nimbusds.jose.crypto.MACSigner;
-import com.nimbusds.jose.crypto.MACVerifier;
-import com.nimbusds.jwt.SignedJWT;
-import com.planb.common.conf.ModuleConfig;
-import com.planb.security.jwt.JwtTokenUtil;
 
 
 @RestController
 @EnableAutoConfiguration
 @ComponentScan({"com.planb"})
 public class RunApplication {
-	
-	@RequestMapping("/home")
-    String home() {
-        return "Hello 333World!--home011:";
-    }
-	
-	public final static String TEST = "xx";
-
-    @RequestMapping(ModuleConfig.TEST + "/xxx3")
-    String index() {
-    	
-    	Map<String, Object> claimMap = new HashMap<String, Object>();
-    	claimMap.put("userId", "123");
-    	String token = JwtTokenUtil.createToken(claimMap);
-    	
-    	JwtTokenUtil.getUserNameFromToken(token);
-    	
-    	
-        return "Hello 333World!--008:" + token;
-    }
 
     public static void main(String[] args) {
 
@@ -58,3 +17,4 @@ public class RunApplication {
     }
 
 }
+
