4648157bcc9dab4ea4856aa6b2671d5da361ea88
deng <6882855@qq.com>
2021-10-14 17:26:38 +0800

    t

--- src/main/java/com/planb/RunApplication.java
+++ src/main/java/com/planb/RunApplication.java
@@ -2,6 +2,7 @@ package com.planb;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -14,13 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 @ComponentScan({"com.planb"})
 public class RunApplication {
+	@Value("${server.servlet.context-path}")
+	private String contextPath;
 	
 	 // spring的上下文 启动时初始化
     public static ConfigurableApplicationContext context;
 
 	@GetMapping("/")
     void home(HttpServletResponse response) throws Exception {
-		response.sendRedirect("/test/testapi/home");
+		response.sendRedirect(contextPath + "/test/testapi/home");
 	}
 	
     public static void main(String[] args) {
